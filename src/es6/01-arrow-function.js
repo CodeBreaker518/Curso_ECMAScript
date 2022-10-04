c1cb981413a3e5a0e9a35f5de0481dadafbc37b0
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-04 03:02:43 -0500

    learning arrow functions

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,9 @@
+function square(num){ //traditional function
+  return num * num
+}
+
+const square = (num) => { //arrow function
+  return num * num
+}
+
+const square = num => num * num //arrow function with return implicit //no need of {} "handlebars"
