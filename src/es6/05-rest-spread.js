3b7878e844f3e1276d8d1e285eb61b40cd3ccd65
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-05 02:20:15 -0500

    learning spread operator & rest parameters

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -1,6 +1,6 @@
 // destructuring
 
-// arrays destructuring
+//arrays destructuring
 let fruits = ['pineapple', 'kiwi', 'strawberry']
 let [a,b,c] = fruits
 console.log(a, fruits[2])
@@ -14,7 +14,7 @@ console.log(fruit)
 //strawberry
 
 
-// object destructuring
+//object destructuring
 let user = {
   username: 'codebreaker',
   age: '20'
@@ -27,7 +27,7 @@ console.log(username, user.age)
 
 
 
-// for-of loop destructuring
+//for-of loop destructuring
 const array = ['a', 'b']
 for (const [index,element] of array.entries()){
   console.log(index,element)
@@ -35,3 +35,39 @@ for (const [index,element] of array.entries()){
   //0 a
   //1 b
 }
+
+//spread operator
+
+let person = {name:'Diego', age: '20'}
+let country = 'MX'
+
+let data = { id: 1,...person, country}
+
+console.log(data)
+//output
+//{ id: 1, name: 'Diego', age: '20', country: 'MX' }
+
+//utiliza el operador de propagación para crear una copia del array que utilice una referencia 
+//en memoria diferente al original, y asi modificar la copia, no el original.
+
+const originalArray = [1,2,3,4,5]
+const copyArray = [...originalArray]
+copyArray[0] = 0 //re declaration of copyArray with 0 on its position 0
+
+console.log(originalArray) // [1,2,3,4,5]
+console.log(copyArray) // [0,2,3,4,5]
+console.log(originalArray === copyArray)  // false
+
+
+// rest parameters
+
+const sum = (num, ...values) =>{
+  console.log(values)
+  console.log(num + values[0])
+  return num + values[0]
+}
+
+sum(1, 1, 2, 3)
+//output
+//[1, 2, 3]
+//2
