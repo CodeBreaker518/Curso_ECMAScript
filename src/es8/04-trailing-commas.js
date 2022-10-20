3ac40c70ac12ca82935574bae4214b97adfca64f
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-20 02:57:06 -0500

    learning trailing commas

---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, , , 6, 7, , 9]
+
+console.log(numbers)
+console.log(numbers.length)
+//output
+// [ 1, 2, 3, <2 empty items>, 6, 7, <1 empty item>, 9 ]
+// 9
+
+// we can put commas like that in an array to create a space in memory even though there's no value (maybe add it on the future)
