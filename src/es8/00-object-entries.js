da06bf8823a364913cde94ef7f181aac04d3fda0
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-20 02:21:13 -0500

    learning object.entries on ES8

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,18 @@
+//Object.blabla converts an object into an array of arrays
+
+const zoo = {
+  lion: '🦁',
+  cat: '🐱',
+  panda: '🐼',
+  fish: '🐟'
+}
+
+console.log(Object.entries(zoo))
+//Output
+// [
+//  [ 'lion', '🦁' ],
+//  [ 'cat', '🐱' ],
+//  [ 'panda', '🐼' ],
+//  [ 'fish', '🐟' ]
+// ]
+
