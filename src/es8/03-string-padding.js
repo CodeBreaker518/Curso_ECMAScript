1b5c2180949700a4685425bd0708cfbf4bdb0660
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-20 02:47:49 -0500

    updated explanation of padStart & padEnd

--- src/es8/03-string-padding.js
+++ src/es8/03-string-padding.js
@@ -2,8 +2,8 @@ const string = "Laugh"
 console.log(string.padStart(10, 'ha'))
 console.log(string.padEnd(10, 'ha'))
 //Output
-//hahahLaugh - 10 characters max start of string
-//Laughhahah - 10 characters max end of string
+//hahahLaugh - 10 characters max fills before string
+//Laughhahah - 10 characters max fills after string
 
 const hello = 'Hello'
 console.log(hello.padStart(6, '_'))
