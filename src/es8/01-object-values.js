351388839b9c246da6203eb06bbea83fe9bec8c7
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-20 02:29:04 -0500

    learning the object values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,12 @@
+const zoo = {
+  lion: '🦁',
+  cat: '🐱',
+  panda: '🐼',
+  fish: '🐟',
+}
+
+console.log(Object.values(zoo))
+//Output
+[ '🦁', '🐱', '🐼', '🐟' ]
+
+// Object.values it only obtains the value of the Object into an array
