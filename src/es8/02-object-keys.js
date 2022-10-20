eace443220e22c3b7afea8a10e7937ce877afb20
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-20 02:30:54 -0500

    learning object keys ES8

---
+++ src/es8/02-object-keys.js
@@ -0,0 +1,12 @@
+const zoo = {
+  lion: '🦁',
+  cat: '🐱',
+  panda: '🐼',
+  fish: '🐟',
+}
+
+console.log(Object.keys(zoo))
+// Output
+// [ 'lion', 'cat', 'panda', 'fish' ]
+
+// Object.keys() converts only the names of the Object into an array
