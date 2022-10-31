b57afdb62908370c1422638c48e8828fa14cf4ad
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-31 12:53:02 -0600

    example of a copy with spread

--- src/es9/01-spread.js
+++ src/es9/01-spread.js
@@ -7,3 +7,11 @@ const {username, ...values} = user
 
 console.log(username)
 console.log(values)
+
+//example of copy with spread operator
+
+const original = { data: [1, [2, 3], 4, 5] }
+const copy = structuredClone(original)
+
+original === copy // false
+original["data"] === copy["data"] // false
