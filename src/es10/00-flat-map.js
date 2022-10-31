98637632382814cfb5fd55ee29a845f82f2d613f
CodeBreaker <diegoperezperez518@gmail.com>
2022-10-31 16:16:25 -0600

    learnin flat & flatMap

---
+++ src/es10/00-flat-map.js
@@ -0,0 +1,13 @@
+// flat
+const array = [1,2,3,4,5,[6,7,8,[9,10,11]]]
+console.log(array.flat(3))
+//output
+// [
+//    1, 2, 3, 4,  5,
+//    6, 7, 8, 9, 10,
+//   11
+// ]
+
+// flat-map
+const array2 = [1,2,3,4,5]
+console.log(array2.flatMap(v => [v, v * 2]))
