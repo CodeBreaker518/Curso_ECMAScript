50400c7f4982857b3940d29e0a143684eff087b0
CodeBreaker <diegoperezperez518@gmail.com>
2022-11-07 15:49:25 -0600

    learning constructor BigInt

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,6 @@
+// constructor BigInt to handle big numbers on JS
+const aBigNumber = 1290381902380912329283n
+const anotherBigNumber = BigInt(1290381902380912329283)
+
+console.log(aBigNumber)
+console.log(anotherBigNumber)
