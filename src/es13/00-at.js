bd0a555e969847ca604a5c13004bd097211e1a94
CodeBreaker <diegoperezperez518@gmail.com>
2022-11-10 18:00:36 -0600

    learning at method

---
+++ src/es13/00-at.js
@@ -0,0 +1,12 @@
+const numbersText = ['one', 'two', 'three', 'four', 'five', 'six']
+
+// traditional form
+console.log(numbersText[numbersText.length - 1])
+// => output
+//six
+
+
+// .at() method
+console.log(numbersText.at(-1))
+// => output
+//six
