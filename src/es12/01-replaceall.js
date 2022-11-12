d73770b1601b3227dd2ae014e7bd8dd5f5de1b4c
CodeBreaker <diegoperezperez518@gmail.com>
2022-11-11 22:17:08 -0600

    updated the output of the example

--- src/es12/01-replaceall.js
+++ src/es12/01-replaceall.js
@@ -1,17 +1,13 @@
 // .replace() - only replaces the first match to replace it encounters
-
 const string = 'JavaScript its an awesome programming language, but you need to focus to learn JavaScript'
-
 const replacedString = string.replace("JavaScript","TypeScript")
 console.log(replacedString)
 // => output
 //TypeScript its an awesome programming language, but you need to focus to learn JavaScript
 
 
-// .replaceAll() as it says, replaces every match it encounters
-
+// .replaceAll() - as it says, replaces every match it encounters
 const string2 = 'JavaScript its an awesome programming language, but you need to focus to learn JavaScript'
-
 const replacedString2 = string2.replaceAll("JavaScript", "TypeScript")
 console.log(replacedString2)
 // => output
@@ -20,15 +16,13 @@ console.log(replacedString2)
 
 
 // Another example with replaceAll()
-
 const inputText = "If you're :) and you know it :clap your hands :clap :clap"
-
 const emojiReplace = (text) => {
   let emojized = ""
   emojized = text.replaceAll(':)', '😀')
   emojized = emojized.replaceAll(':clap', '👏🏻')
-
   return emojized
 }
-
 console.log(emojiReplace(inputText))
+// => output
+// If you're 😀 and you know it 👏🏻 your hands 👏🏻 👏🏻
